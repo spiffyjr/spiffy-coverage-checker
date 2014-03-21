6c3bdf437d71639000dc1c114d247edd56cc6426
Kyle Spraggs <theman@spiffyjr.me>
2014-03-21 08:59:42 -0500

    Initial commit.

---
+++ coverage-checker.php
@@ -0,0 +1,29 @@
+<?php
+// Script copied and slightly modified from Ocramius
+// http://ocramius.github.io/blog/automated-code-coverage-check-for-github-pull-requests-with-travis/
+
+$inputFile  = $argv[1];
+$percentage = min(100, max(0, (int) isset($argv[2]) ? $argv[2] : 0));
+
+if (!file_exists($inputFile)) {
+    throw new InvalidArgumentException('Invalid input file provided');
+}
+
+$xml             = new SimpleXMLElement(file_get_contents($inputFile));
+$metrics         = $xml->xpath('//metrics');
+$totalElements   = 0;
+$checkedElements = 0;
+
+foreach ($metrics as $metric) {
+    $totalElements   += (int) $metric['elements'];
+    $checkedElements += (int) $metric['coveredelements'];
+}
+
+$coverage = ($checkedElements / $totalElements) * 100;
+
+if ($coverage < $percentage) {
+    echo 'Code coverage is ' . $coverage . '%, which is below the accepted ' . $percentage . '%' . PHP_EOL;
+    exit(1);
+}
+
+echo 'Code coverage is ' . $coverage . '% - OK!' . PHP_EOL;
